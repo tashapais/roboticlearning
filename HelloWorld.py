6c90d391a6f0025aab88cc413e582e002e176c58
Tasha Pais <tashapais@gmail.com>
2022-10-10 16:31:53 -0400

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,19 @@
+import pybullet as p
+import time
+import pybullet_data
+
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+startPos = [0,0,1]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("assets/fetch.urdf",startPos, startOrientation)
+#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.) #each time step on pybullet is 1/240 seconds
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+
+print(cubePos,cubeOrn)
+p.disconnect()
