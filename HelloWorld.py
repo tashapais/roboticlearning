4ddd84e0f8b733028920db890ee12de76c981f87
Tasha Pais <32210691+tashapais@users.noreply.github.com>
2022-10-31 16:27:33 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,19 +1,38 @@
-import pybullet as p
-import time
-import pybullet_data
+from ai2thor.controller import Controller
 
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
-planeId = p.loadURDF("plane.urdf")
-startPos = [0,0,1]
-startOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("assets/fetch.urdf",startPos, startOrientation)
-#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-for i in range (10000):
-    p.stepSimulation()
-    time.sleep(1./240.) #each time step on pybullet is 1/240 seconds
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+controller = Controller(
+    agentMode="default",
+    visibilityDistance=1.5,
+    scene="FloorPlan212",
 
-print(cubePos,cubeOrn)
-p.disconnect()
+    # step sizes
+    gridSize=0.25,
+    snapToGrid=True,
+    rotateStepDegrees=90,
+
+    # image modalities
+    renderDepthImage=False,
+    renderInstanceSegmentation=False,
+
+    # camera properties
+    width=1000,
+    height=1000,
+    fieldOfView=90
+)
+
+#controller.start()
+event= None
+while True :
+    choice= input()
+    if choice == "w":
+        event= controller.step("MoveAhead")
+    if choice == "s":
+        event= controller.step("MoveBack")
+    if choice == "a":
+        event= controller.step("MoveLeft")
+    if choice == "d":
+        event= controller.step("MoveRight")
+    
+    print (event.metadata["lastActionSuccess"])
+    print (event.metadata["errorMessage"])
+    controller.step("Done")
