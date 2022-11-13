f7f0c3647b6999474cc01158189ab98543565606
Nikhilesh Belulkar <nb2953@columbia.edu>
2022-11-12 20:02:26 -0500

    adding multiple objects to the map

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,9 @@
 from ai2thor.controller import Controller
-
+from pprint import pprint 
 controller = Controller(
     agentMode="default",
     visibilityDistance=1.5,
-    scene="FloorPlan212",
+    scene="FloorPlan30",
 
     # step sizes
     gridSize=0.25,
@@ -19,8 +19,59 @@ controller = Controller(
     height=1000,
     fieldOfView=90
 )
+controller.step(
+    action='SetObjectPoses',
+    objectPoses=[{
+        "objectName":"Mug_843dabdf",
+        "rotation": {
+            "y": 0,
+            "x": 0,
+            "z": 0
+        },
+        "position":{
+            "y":"1.401",
+            "x":"1.872",
+            "z":"-1.26"
+        }
+    },
+    {
+        "objectName":"Ladle_7069fa47",
+        "rotation": {
+            "y": 0,
+            "x": 0,
+            "z": 0
+        },
+        "position":{
+            "y":"1.404",
+            "x":"1.878",
+            "z":"-1.26"
+        }
+    },
+    {
+        "objectName":"Kettle_9509bd9c",
+        "rotation": {
+            "y": 0,
+            "x": 0,
+            "z": 0
+        },
+        "position":{
+            "y":"1.407",
+            "x":"1.870",
+            "z":"-1.26"
+        }
+    }
+    ]
+)
 
-#controller.start()
+
+for obj in controller.last_event.metadata["objects"]:
+    pprint("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    pprint(obj["objectType"])
+    pprint(obj["name"])
+    pprint(obj["objectId"])
+    pprint(obj["position"])
+    pprint("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+
 event= None
 while True :
     choice= input()
@@ -32,6 +83,11 @@ while True :
         event= controller.step("MoveLeft")
     if choice == "d":
         event= controller.step("MoveRight")
+    if choice == "o":
+        event = controller.step(action="OpenObject",
+                                objectId="Cabinet|+01.40|+01.87|-01.26", 
+                                openness=1,
+                                forceAction=False)
     
     print (event.metadata["lastActionSuccess"])
     print (event.metadata["errorMessage"])
---
+++ starting_thor.py
@@ -0,0 +1,16 @@
+from ai2thor.controller import Controller 
+from ai2thor.platform import CloudRendering
+
+controller = Controller()
+event = controller.step("MoveAhead")
+
+
+
+
+
+
+
+
+
+
+
